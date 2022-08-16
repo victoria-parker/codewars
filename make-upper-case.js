6affd3306cc0812814e2bf9db34f57448fcd7010
victoria-parker <victoriaparkerb@gmail.com>
2022-08-16 10:02:36 +0200

    i make upper case added

---
+++ make-upper-case.js
@@ -0,0 +1,11 @@
+/* MakeUpperCase
+DESCRIPTION:
+Write a function which converts the input string to uppercase.
+
+FUNDAMENTALS STRINGS
+*/
+
+function makeUpperCase(str) {
+    //I use the toUpperCase method to change the string to upper case
+    return str.toUpperCase()
+  }
