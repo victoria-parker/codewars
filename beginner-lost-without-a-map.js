2426543f009b8ff413e976fa8ef5fde8ef3bcf2a
victoria-parker <victoriaparkerb@gmail.com>
2022-09-20 17:09:27 +0200

    lost without a map added

---
+++ beginner-lost-without-a-map.js
@@ -0,0 +1,12 @@
+/* Beginner - Lost Without a Map
+DESCRIPTION:
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+function maps(x){
+    //I use the map method to return a copy of the original array where the values are multiply by two
+    return x.map(val=>val*2)
+  }
