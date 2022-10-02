6d7a9d9e4283d503822b0c4bab8ac87fa3cd26c2
victoria-parker <victoriaparkerb@gmail.com>
2022-10-02 02:15:47 +0200

    printing array elements with commas added

---
+++ printing-array-elements-with-comma-delimiters.js
@@ -0,0 +1,13 @@
+/* Printing Array elements with Comma delimiters
+DESCRIPTION:
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level
+*/
+
