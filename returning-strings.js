bbb43e93cffa4eddcc3bfa08cf4504b4f494dd35
victoria-parker <victoriaparkerb@gmail.com>
2022-10-03 19:27:39 +0200

    returning string added

---
+++ returning-strings.js
@@ -0,0 +1,13 @@
+/* Returning Strings
+DESCRIPTION:
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+STRINGS FUNDAMENTALS 
+*/
+
+function greet(name){
+    //It uses a template literal to return the string with the name provided
+    return `Hello, ${name} how are you doing today?`
+  }
