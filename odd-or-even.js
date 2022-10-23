3ba399c9063b668a033c9f57c18f02395ade51fd
victoria-parker <victoriaparkerb@gmail.com>
2022-10-23 10:02:36 +0200

    odd or even added

---
+++ odd-or-even.js
@@ -0,0 +1,30 @@
+/* Odd or Even?  
+
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+Have fun!
+
+FUNDAMENTALSARRAYS
+
+*/
+
+function oddOrEven(array) {
+    //First it gets the sum using the reduce method. important put initial value in case the array is empty
+   const sum=array.reduce((acc,val)=>acc+val,0)
+   //then it checks if its odd or even and returns the matching string
+   return sum % 2 == 0 ? 'even' : 'odd'
+ }
