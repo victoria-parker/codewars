47948377d427f09f517c6bf58d386b0858202a44
victoria-parker <victoriaparkerb@gmail.com>
2022-11-01 12:52:30 +0100

    find numbers refactored

--- Find-numbers-which-are-divisible-by-given-number.js
+++ Find-numbers-which-are-divisible-by-given-number.js
@@ -4,15 +4,5 @@ Example(Input1, Input2 --> Output)
 [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6] */
 
 function divisibleBy(numbers, divisor){
-    //  First I create acc variable to store the elements that are divisible by the divisor
-      let acc=[]
-     
-    //  Second I iterate through the array and check if the pass the condition, if they do I store them in the acc array
-     for(let i=0;i<numbers.length;i++){
-       if (numbers[i] % divisor === 0){
-         acc.push(numbers[i])
-       }
-     }
-    //   Third I return the array with the elements that passed the condition 
-      return acc
-    }
+  return numbers.filter(val=>val % divisor == 0)
+}
