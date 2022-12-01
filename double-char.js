88365ca9e53a2ace774e618090c26f722821b8e3
victoria-parker <victoriaparkerb@gmail.com>
2022-12-01 22:15:22 +0100

    double char added

---
+++ double-char.js
@@ -0,0 +1,15 @@
+/* Double Char
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+
+FUNDAMENTALSSTRINGS
+*/
+
+function doubleChar(str) {
+    return str.split('').map(e=>e.concat(e)).join('')
+}
