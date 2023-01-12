725b924f3755c1609c091be901424c6d41d36c89
victoria-parker <victoriaparkerb@gmail.com>
2023-01-12 16:00:52 +0100

    indexed cap added

---
+++ indexed-capitalization.js
@@ -0,0 +1,17 @@
+/* Indexed capitalization
+
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits
+*/
+
+function capitalize(s,arr){
+  
+    return [...s].map((char,index)=>arr.includes(index) ? char.toUpperCase() : char).join("")
+    
+};
+
