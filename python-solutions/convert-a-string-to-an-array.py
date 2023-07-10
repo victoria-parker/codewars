5a9c36eab6184b12c6a4e994ab3db860bf2a7295
victoria-parker <victoriaparkerb@gmail.com>
2023-07-10 22:10:32 +0200

    convert string to an array added

---
+++ python-solutions/convert-a-string-to-an-array.py
@@ -0,0 +1,12 @@
+#Convert a string to an array
+# DESCRIPTION:
+# Write a function to split a string and convert it into an array of words.
+
+# Examples (Input ==> Output):
+# "Robin Singh" ==> ["Robin", "Singh"]
+
+# "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# ARRAYSSTRINGSFUNDAMENTALS
+
+def string_to_array(s):
+    return s.split(' ')
