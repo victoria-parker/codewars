10ab3782200d34358f1cd9dae4ca560afbe6e9b6
victoria-parker <victoriaparkerb@gmail.com>
2023-09-25 20:16:03 +0200

    Beginner - Lost Without a Map added

---
+++ python-solutions/beginner-lost-without-a-map.py
@@ -0,0 +1,12 @@
+# Beginner - Lost Without a Map
+# DESCRIPTION:
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+# FUNDAMENTALSARRAYS
+
+def maps(a):
+    return [num*2 for num in a]
