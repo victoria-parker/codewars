1e0d4ce0f1152cc541f0925d4eea72645027d90d
victoria-parker <victoriaparkerb@gmail.com>
2023-10-03 20:54:39 +0200

    disemvowel trolls added

---
+++ python-solutions/disemvowel-trolls.py
@@ -0,0 +1,16 @@
+# Disemvowel Trolls
+# DESCRIPTION:
+# Trolls are attacking your comment section!
+
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+# Note: for this kata y isn't considered a vowel.
+
+# STRINGSREGULAR EXPRESSIONSFUNDAMENTALS
+
+def disemvowel(string_):
+    return ''.join([c for c in string_ if c not in 'AEIOUaeiou'])
