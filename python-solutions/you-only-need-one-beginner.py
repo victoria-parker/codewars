910dd18e9f6ff95c652a23317431c013434e0532
victoria-parker <victoriaparkerb@gmail.com>
2023-10-03 20:38:28 +0200

    you only need one added

---
+++ python-solutions/you-only-need-one-beginner.py
@@ -0,0 +1,13 @@
+# You only need one - Beginner
+
+# DESCRIPTION:
+# You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+# Array can contain numbers or strings. X can be either.
+
+# Return true if the array contains the value, false if not.
+
+# FUNDAMENTALSSTRINGSARRAYS
+
+def check(seq, elem):
+    return elem in seq
