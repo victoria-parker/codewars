c55214fbb8cbc3323eb7f9b7b448b2f942d2c5b8
victoria-parker <victoriaparkerb@gmail.com>
2023-10-21 13:44:58 +0100

    remove anchor from url added

---
+++ python-solutions/remove-anchor-from-url.py
@@ -0,0 +1,12 @@
+# Remove anchor from URL
+
+# DESCRIPTION:
+# Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+# Examples
+# "www.codewars.com#about" --> "www.codewars.com"
+# "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+def remove_url_anchor(url):
+    lst = url.split('#')
+    return lst[0]
