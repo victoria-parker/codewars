49cd79c0b002843d9eb32737b1e462775724b82e
victoria-parker <victoriaparkerb@gmail.com>
2023-10-29 15:32:56 +0100

    greet-me added

---
+++ python-solutions/greet-me.py
@@ -0,0 +1,12 @@
+# Greet Me
+# DESCRIPTION:
+# Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+# Example:
+
+# "riley" --> "Hello Riley!"
+# "JACK"  --> "Hello Jack!"
+# STRINGSFUNDAMENTALS
+
+def greet(name): 
+    return f'Hello {name.lower().capitalize()}!'
