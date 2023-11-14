d098a90751b2a63c9b24c70c4e44ff506c8d4844
victoria-parker <victoriaparkerb@gmail.com>
2023-11-14 22:50:21 +0100

    spacify added

---
+++ python-solutions/spacify.py
@@ -0,0 +1,10 @@
+# Spacify
+
+# DESCRIPTION:
+# Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+# spacify("hello world") # returns "h e l l o   w o r l d"
+# FUNDAMENTALSSTRINGSARRAYS
+
+def spacify(string):
+    return ' '.join([*string])
