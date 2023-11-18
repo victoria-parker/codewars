e1bac40e72265013aaf3cac8e92e470b77bb6a94
victoria-parker <victoriaparkerb@gmail.com>
2023-11-18 14:02:24 +0100

    alphabet-symmetry added

---
+++ python-solutions/alphabet-symmetry.py
@@ -0,0 +1,25 @@
+# Alphabet symmetry
+
+# DESCRIPTION:
+# Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+# Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+# solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+# See test cases for more examples.
+
+# Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+
+# Good luck!
+
+def solve(strings : list[str]) -> list[int]:
+    answers = []
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for str in strings:
+        answer = 0
+        for i in range(len(str)):
+            if i < len(alphabet):
+                if str[i].lower() == alphabet[i]:
+                    answer+=1
+        answers.append(answer)
+    return answers
